86472b0ca0b5bc648826dfd98bfb94bff9b70a50
David <dgreenfield0@gmail.com>
2012-06-02 17:47:17 -0700

    Updated martusActivity

--- matrus-android/src/com/martus/android/MatrusActivity.java
+++ matrus-android/src/com/martus/android/MatrusActivity.java
@@ -1,13 +1,37 @@
 package com.martus.android;
 
+import org.martus.clientside.ClientSideNetworkHandlerUsingXmlRpcForNonSSL;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MatrusActivity extends Activity {
-    /** Called when the activity is first created. */
+
+	ClientSideNetworkHandlerUsingXmlRpcForNonSSL client = new ClientSideNetworkHandlerUsingXmlRpcForNonSSL("66.201.46.82");
+	String response;
+	TextView textview;
+
+	    
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        textview = new TextView(this); 
+    	textview=(TextView)findViewById(R.id.response); 
+	    
+        final Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	response = client.ping();
+            	textview.setText("response:" +response);
+            	
+            }
+        });
     }
+    
 }
