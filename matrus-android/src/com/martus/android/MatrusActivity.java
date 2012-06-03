c3574cd3033be4876773b7058ced90eb8b20056f
David <dgreenfield0@gmail.com>
2012-06-03 04:09:57 -0700

    Working on testing ping in mainActivity

--- matrus-android/src/com/martus/android/MatrusActivity.java
+++ matrus-android/src/com/martus/android/MatrusActivity.java
@@ -1,16 +1,27 @@
 package com.martus.android;
 
+import java.net.URI;
+import java.net.URL;
+import java.util.Vector;
+
+import org.apache.xmlrpc.client.XmlRpcClient;
+import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
 import org.martus.clientside.ClientSideNetworkHandlerUsingXmlRpcForNonSSL;
+//import org.xmlrpc.android.XMLRPCClient;
+//import org.xmlrpc.android.XMLRPCException;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MatrusActivity extends Activity {
 
-	ClientSideNetworkHandlerUsingXmlRpcForNonSSL client = new ClientSideNetworkHandlerUsingXmlRpcForNonSSL("66.201.46.82");
+	String serverURL = "http://66.201.46.82:988/RPC2";
+	XmlRpcClient client = new XmlRpcClient();
+//	ClientSideNetworkHandlerUsingXmlRpcForNonSSL client = new ClientSideNetworkHandlerUsingXmlRpcForNonSSL("66.201.46.82");
 	String response;
 	TextView textview;
 
@@ -27,9 +38,20 @@ public class MatrusActivity extends Activity {
         final Button button = (Button) findViewById(R.id.button1);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-            	response = client.ping();
-            	textview.setText("response:" +response);
-            	
+            	try {
+            		Log.v("xmlrpc", "calling: "+serverURL);
+            		XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+            		config.setServerURL(new URL(serverURL));
+            		XmlRpcClient client = new XmlRpcClient();
+            		client.setConfig(config);
+            		Vector params = new Vector();
+            		String result = (String) client.execute("MartusServer.ping", params);
+                  	textview.setText("response: " +result);
+            		Log.v("xmlrpc", "response: "+result);
+				} catch (Exception e) {
+					Log.e("error", "xmlrpc call failed");
+					e.printStackTrace();
+				}
             }
         });
     }
